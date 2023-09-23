371ca41b726c75dce7c3c324f1825232d61cf0f5
kimwangchong <kimwangchong@didiglobal.com>
2023-09-23 22:33:49 +0800

    update

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "keverjs",
   description: "keverjs docs",
-  base: 'docs',
   themeConfig: {
     nav: [
       { text: 'Home', link: '/' },
