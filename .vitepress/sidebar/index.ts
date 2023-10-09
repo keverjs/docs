b25568b62b6dfb0a324c031b276fbb076b11c7a8
kimwangchong <kimwangchong@didiglobal.com>
2023-10-09 17:16:03 +0800

    feat: update

--- .vitepress/sidebar/index.ts
+++ .vitepress/sidebar/index.ts
@@ -1,11 +1,24 @@
 const api = [
   {
-    title: "API",
-    collapsable: false,
-    children: [
-      ''
+    text: 'Introduction',
+    collapsed: false,
+    items: [
+      { text: 'What is Keverjs?', link: '/guide/introduction/way' },
+      { text: 'Getting Started', link: '/guide/introduction/getting-started' },
     ]
   },
+  {
+    text: 'Usage',
+    collapsed: false,
+    items: [
+      { text: 'Create web server', link: '/guide/use/first' },
+      { text: 'CreateApp', link: '/guide/use/createApp' },
+      { text: 'Controllers', link: '/guide/use/controller' },
+      { text: 'Dependency injection', link: '/guide/use/service' },
+      { text: 'Middlewares', link: '/guide/use/middleware' },
+      { text: 'Runtime enhancement', link: '/guide/use/enhance' },
+    ]
+  }
 ]
 
 const guide = [
